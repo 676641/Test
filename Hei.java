0600e77e89573a06c3137ae353da4a6ccac65c59
676641 <676641@stud.hvl.no>
2023-08-22 14:53:05 +0200

    navn

--- Hei.java
+++ Hei.java
@@ -1,7 +1,6 @@
 
 class Hei{
     public static void main(String[] args) {
-        SkrivUt.Print("Hei");
-        SkrivUt.Print("Tulling");
+        SkrivUt.PrintNavn("Mats", "Mus");
     }
 }
--- SkrivUt.java
+++ SkrivUt.java
@@ -1,5 +1,5 @@
 public class SkrivUt {
-    public static void Print(String tekst){
-        System.out.println(tekst);
+    public static void PrintNavn(String navn1, String navn2){
+        System.out.println(navn1+" "+navn2);
     }
 }
